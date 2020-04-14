9c9698bf675aaf288fb08de7eacd833fcf60e97d
iyinraphael <iyinraphael@gmail.com>
2020-04-13 19:12:01 -0700

    storyboard setup

---
+++ Swift Vocabulary/Swift Vocabulary/DefinitionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DefinitionViewController.swift
+//  Swift Vocabulary
+//
+//  Created by Iyin Raphael on 4/13/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var definitionTextView: UITextView!
+    
+}
---
+++ Swift Vocabulary/Swift Vocabulary/Vocabulary.swift
@@ -0,0 +1,16 @@
+//
+//  Vocabulary.swift
+//  Swift Vocabulary
+//
+//  Created by Iyin Raphael on 4/13/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
+
+struct VocabularyWord {
+    
+    var word: String
+    var definition: String
+    
+}
